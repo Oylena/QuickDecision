171466cd91c9fc5d761d9e00737994d2d0a24a10
Sasha <dimurchik@ukr.net>
2014-01-11 14:12:15 +0200

    add highcharts

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jquery-rails'
 
 gem "bower-rails", "~> 0.6.1"
 
+gem "highcharts-rails", "~> 3.0.0"
+
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require highcharts
+//= require highcharts/highcharts-more # to get the new features in 2.3.0
+//= require highcharts/adapters/mootools-adapter
+//= require highcharts/adapters/prototype-adapter
+//= require highcharts/adapters/standalone-framework
